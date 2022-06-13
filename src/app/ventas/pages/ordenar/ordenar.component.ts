171e6b54aeceeb5e0ee021513275264def9ac6b0
Juan David Ñustes <Jnustesn@contratista.epm.co>
2022-06-13 10:11:56 -0500

    Se agregan Pipes de ordenar y conversion si vuela o no

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,15 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent  {
 
   mayusculas: boolean=true;
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  ordenarPor: string = '';
 
+  heroes: Heroe[] = [
+    {
+      nombre: 'superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'DareDevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
+  
   cambiar(){
     this.mayusculas = !this.mayusculas
+  }
+
+  cambiarOrden(valor: string){
+    this.ordenarPor= valor;
   }
 }
